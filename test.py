46b00cf843c934a8dd1f49e112b62e15e78ca358
WeyzyG <95768748+WeyzyG@users.noreply.github.com>
2021-12-15 18:07:06 +0300

    Update test.py

--- test.py
+++ test.py
@@ -23,6 +23,8 @@ class SomeTest(unittest.TestCase):
     def test_mean(self):  # Дополнительный тест
         self.assertEqual(find_mean(), mean(data))
 
+if __name__ == '__main__':
+    unittest.main()
 
 t_2 = time()
 work_time = t_2 - t_1
