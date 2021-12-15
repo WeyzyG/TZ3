d67cffc0b7510be4211d4aaaa1814b3c80181183
WeyzyG <95768748+WeyzyG@users.noreply.github.com>
2021-12-15 17:42:28 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+import unittest, math
+from time import time
+from statistics import mean
+from myscript import find_max, find_min, find_sum, find_prod, find_mean, data
+
+t_1 = time()
+
+
+class SomeTest(unittest.TestCase):
+
+    def test_max(self):
+        self.assertEqual(find_max(), max(data))
+
+    def test_min(self):
+        self.assertEqual(find_min(), min(data))
+
+    def test_sum(self):
+        self.assertEqual(find_sum(), sum(data))
+
+    def test_prod(self):
+        self.assertEqual(find_prod(), math.prod(data))
+
+    def test_mean(self):  # Дополнительный тест
+        self.assertEqual(find_mean(), mean(data))
+
+
+t_2 = time()
+work_time = t_2 - t_1
+print(f"Время: {work_time} сек.")
